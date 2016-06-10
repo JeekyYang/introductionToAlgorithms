0d089f77049391246b9b7cf882bce4b3215ef62f
jileiYang <jileiyang@creditease.cn>
2016-06-11 01:21:56 +0800

    1. change int A[] to vector<int> &A 2. add merge sort

--- sort.cpp
+++ sort.cpp
@@ -1,8 +1,36 @@
 /*
+* add main function for test all the sort algorithm
+*/
+#include<vector>
+#include<time.h>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+
+void printArray(vector<int> data) {
+	for(int i = 0; i < data.size(); i++) {
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+}
+
+int main(){
+	vector<int> testData(10);
+	srand((unsigned int)time(NULL));
+	for(int i = 0; i < 10; i++) {
+		testData[i] = rand()%100;
+	}
+	printArray(testData);//print the initialized array
+	//sort the arrat
+	printArray(testData);//print the result of sorted array
+	return 0;
+}
+
+/*
 * implement quick sort
 */
 
-int partition(int A[], int p, int r) {
+int partition(vector<int> &A, int p, int r) {
 	int x = A[r];
 	int i = p - 1;
 	for (int j = p; j < r; j++) {
@@ -14,7 +42,7 @@ int partition(int A[], int p, int r) {
 	return i+1;	
 }
 
-void sort_quick(int A[], int p, int r) {
+void sort_quick(vector<int> &A, int p, int r) {
 	if (p < r) {
 		int pivot = partition(A, p, r);
 		sort_quick(A, p, pivot - 1);
@@ -27,7 +55,7 @@ void sort_quick(int A[], int p, int r) {
 * implement max heap sort
 */
 
-void maxHeapify(int A[], int heapsize, int i) {
+void maxHeapify(vector<int> &A, int heapsize, int i) {
 	int left = 2 * i + 1;
 	int right = 2 * i + 2;
 	int largest = i;
@@ -43,7 +71,7 @@ void maxHeapify(int A[], int heapsize, int i) {
 	}
 }
 
-void buildMaxHeap(int A[], int heapsize) {
+void buildMaxHeap(vector<int> &A, int heapsize) {
 	int i = (heapsize - 1) / 2;
 	while(i >= 0) {
 		maxHeapify(A, heapsize, i);
@@ -51,10 +79,46 @@ void buildMaxHeap(int A[], int heapsize) {
 	}
 }
 
-void sort_heap(int A[], int heapsize) {
+void sort_heap(vector<int> &A, int heapsize) {
 	buildHeap(A, heapsize);
 	for(int i = heapsize - 1; i > 0; i--) {
 		swap(A[0], A[i]);
 		maxHeapify(A, i, 0);
 	}
 }
+
+/*
+* implement merge sort
+*/
+void msort(vector<int> &A, int p, int r) {
+	if (p < r) {
+		int q = p + (r - p)/2;
+		msort(A, p, q);
+		msort(A, q + 1, r);
+		merge(A, p, q, r);
+	}
+}
+void merge(vector<int> &A, int p, int q, int r) {
+	int len1 = q - p + 1;
+	int len2 = r - q;
+	vector<int> left(len1 + 1);
+	vector<int> right(len2 + 1);
+	left[len1] = right[len2] = INT_MAX;// set the sentinel
+	for(int i = 0; i < len1; i++) {
+		left[i] = A[p + i];
+	}
+	for(int j = 0; j < len2; j++) {
+		right[j] = A[q + 1 + j];	
+	}	
+	int i = j = 0;
+	for(int k = p; k < q+1; k++) {
+		if (left[i] < right[j]) {
+			A[k] = left[i++];
+		} else {
+			A[k] = right[j++];
+		}
+	}
+}
+/*
+* implement shell sort
+*/
